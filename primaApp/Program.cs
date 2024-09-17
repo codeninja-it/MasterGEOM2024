613d311ac876d3e39bfbd7b8bd6cadad5f0d5647
Daniele Simoncini <simoncini@codeninja.it>
2024-09-17 19:11:46 +0200

    Aggiungere i file di progetto.

---
+++ primaApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// 1) mi faccio dire il nome del mio utente
+Console.WriteLine("Come ti chiami?");
+var nomeUtente = "";
+nomeUtente = Console.ReadLine();
+// 2) attacco il nome del mio utente ad un saluto
+var messaggio = "Ciao " + nomeUtente;
+// 3) saluto il mio utente
+Console.WriteLine(messaggio);
